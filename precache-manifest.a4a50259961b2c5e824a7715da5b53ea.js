10d770e3dc43f33224f4484462efa9c6d5e5c5c0
grapedge <grapedge@gmail.com>
2020-02-03 22:55:15 +0800

    add

---
+++ precache-manifest.a4a50259961b2c5e824a7715da5b53ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742c93be45b239358bb4d1aa2b98a592",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c72703c4c831dfadbf51",
+    "url": "./static/css/main.fe747915.chunk.css"
+  },
+  {
+    "revision": "b9aef73160cf44b65ff0",
+    "url": "./static/js/2.16032329.chunk.js"
+  },
+  {
+    "revision": "c72703c4c831dfadbf51",
+    "url": "./static/js/main.4d1b78ba.chunk.js"
+  },
+  {
+    "revision": "b30de4afa6b79a6f6e11",
+    "url": "./static/js/runtime-main.50999873.js"
+  }
+]);
